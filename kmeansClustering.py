13460388241e5b10db4124d553d9fe3472d20c06
ErinRJ <erin.rutkowskijones@uoit.net>
2020-04-09 15:56:14 -0400

    created basic 3d kmeans program

--- kmeansClustering.py
+++ kmeansClustering.py
@@ -2,23 +2,30 @@ import copy
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class Centroid:
     def __init__(self, colour):
         self.x = np.random.randint(0, 100)
         self.y = np.random.randint(0, 100)
+        self.z = np.random.randint(0, 100)
         self.colour = colour
         self.points = []
 
-    def add_point(self, x, y):
-        self.points.append([x, y])
+    def add_point(self, x, y, z):
+        self.points.append([x, y, z])
 
     def update_centroid(self):
         xs = self.get_xs()
         ys = self.get_ys()
+        zs = self.get_zs()
         self.x = sum(xs) / len(xs)
         self.y = sum(ys) / len(ys)
+        self.z = sum(zs) / len(zs)
+
+    def clear_points(self):
+        self.points = []
 
     def get_xs(self):
         temp_array = []
@@ -26,14 +33,20 @@ class Centroid:
             temp_array.append(self.points[i][0])
         return temp_array
 
+
     def get_ys(self):
         temp_array = []
         for i in range(0, len(self.points)):
             temp_array.append(self.points[i][1])
         return temp_array
 
+    def get_zs(self):
+        temp_array = []
+        for i in range(0, len(self.points)):
+            temp_array.append(self.points[i][2])
+        return temp_array
 
-def initialize_components(x, y):
+def initialize_components(x, y, z):
     # randomly initialize centroid values
     k = 4
     centroids = []
@@ -44,23 +57,26 @@ def initialize_components(x, y):
     # print(centroids)
 
     # assign each variable to the closest centroid
-    find_closest_centroid(x, y, centroids)
+    find_closest_centroid(x, y, z, centroids)
     # loop through each variables, find the centroid with the smallest euclidean distance
     return centroids
 
 
-def find_closest_centroid(x, y, centroids):
+def find_closest_centroid(x, y, z, centroids):
+    # first clear up the points belonging to each centroid
+    for centroid in centroids:
+        centroid.clear_points()
     for i in range(0, len(x)):
         smallest_euclidean_distance = 10000
         chosen_centroid = 0
         # loop through each of the centroid's coordinates, calculate the distance
         for j in range(0, len(centroids)):
-            euclidean_distance = math.sqrt((((x[i] - centroids[j].x) ** 2) + ((y[i] - centroids[j].y) ** 2)))
+            euclidean_distance = math.sqrt((((x[i] - centroids[j].x) ** 2) + ((y[i] - centroids[j].y) ** 2) + ((z[i] - centroids[j].z) ** 2)))
             if euclidean_distance < smallest_euclidean_distance:
                 smallest_euclidean_distance = euclidean_distance
                 chosen_centroid = j
         # add this value to the centroid's cluster
-        centroids[chosen_centroid].add_point(x[i], y[i])
+        centroids[chosen_centroid].add_point(x[i], y[i], z[i])
     return centroids
 
 
@@ -70,15 +86,17 @@ def verify_centroids(centroids):
         if len(element.get_xs()) == 0:
             return False
     return True
-    # update the centroid to the mean of the cluster
+
 
 
 def plot_final_results(centroids):
     # define the matplotlib properties
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
     plt.figure(figsize=(5, 5))
     for i in range(0, len(centroids)):
-        plt.scatter(centroids[i].get_xs(), centroids[i].get_ys(), color=centroids[i].colour)
-        plt.scatter(centroids[i].x, centroids[i].y, color=centroids[i].colour, marker="^")
+        ax.scatter(centroids[i].get_xs(), centroids[i].get_ys(), centroids[i].get_zs(), color=centroids[i].colour)
+        ax.scatter(centroids[i].x, centroids[i].y, centroids[i].z, color=centroids[i].colour, marker="^")
     # show the final plot
     plt.show()
 
@@ -89,13 +107,15 @@ if __name__ == '__main__':
     # take in the data
     x_values = [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72]
     y_values = [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
+    z_values = [40, 15, 11, 53, 12, 4, 99, 75, 33, 81, 66, 45, 15, 19, 83, 91, 100, 4, 34]
+
 
     # initialize all of the elements
     keep_looping = True
     list_of_centroids = []
 
     while keep_looping:
-        list_of_centroids = initialize_components(x_values, y_values)
+        list_of_centroids = initialize_components(x_values, y_values, z_values)
 
         # make sure each centroid has at least one point within its cluster
         verification = verify_centroids(list_of_centroids)
@@ -106,17 +126,17 @@ if __name__ == '__main__':
 
     finding_optimal_centroids = True
     while finding_optimal_centroids:
+        print("new loop")
         # log centroid results
         old_centroids = copy.deepcopy(list_of_centroids)
         # update the centroid to the mean of the cluster
         for centroid in list_of_centroids:
             centroid.update_centroid()
-        print("AFTER UPDATE")
 
         # check if the centroids have changed since last time
         changed_flag = False
         for i in range(0, len(old_centroids)):
-            if (list_of_centroids[i].x != old_centroids[i].x) | (list_of_centroids[i].y != old_centroids[i].y):
+            if (list_of_centroids[i].x != old_centroids[i].x) | (list_of_centroids[i].y != old_centroids[i].y) | (list_of_centroids[i].z != old_centroids[i].z):
                 changed_flag = True
 
         # if the centroids have not changed, stop looping through
@@ -124,7 +144,8 @@ if __name__ == '__main__':
             finding_optimal_centroids = False
 
         # recalculate which points belong to which cluster
-        list_of_centroids = find_closest_centroid(x_values, y_values, list_of_centroids)
-
+        list_of_centroids = find_closest_centroid(x_values, y_values, z_values, list_of_centroids)
+    for centroid in list_of_centroids:
+        print(centroid.points)
     # plot the final graph
     plot_final_results(list_of_centroids)
